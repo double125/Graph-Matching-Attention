3f9bfd04846bc334ddd114cae0af555d2c1e78dc
CaoJianjian <1300061497@qq.com>
2023-02-16 17:07:55 +0800

    Update build_image_feat.sh

--- VQAdata_process/visual_100/build_image_feat.sh
+++ VQAdata_process/visual_100/build_image_feat.sh
@@ -1,2 +1,2 @@
-#nohup python3 preprocess_image.py --data trainval >./log_trainval.txt 2>&1 &
+nohup python3 preprocess_image.py --data trainval >./log_trainval.txt 2>&1 &
 nohup python3 preprocess_image.py --data test >./log_test.txt 2>&1 &
